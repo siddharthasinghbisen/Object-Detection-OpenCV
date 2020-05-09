f9144594499a257c78e2084741c0cb9f8e3ba1cd
pscrusher101 <siddharthasingh_besen@yahoo.in>
2020-05-09 04:08:53 +0200

    Added video Feed

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-hello sid
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+
+
+class ObjectDetection:
+
+    def GetVideo():
+        video = cv2.VideoCapture(0)
+        return video
+
+    def display(video):
+
+        while(True):
+            ret, Frame = video.read()
+            cv2.imshow('frame', Frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        video.release()
+        cv2.destroyAllWindows()
+
+    if __name__ == '__main__':
+        vid = GetVideo()
+        display(vid)
