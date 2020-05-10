df51e9d0d1c1648261e38ea48c5b30adabc9d035
pscrusher101 <siddharthasingh_besen@yahoo.in>
2020-05-10 02:14:52 +0200

    frames converteed to hsv and masked

--- main.py
+++ main.py
@@ -14,10 +14,22 @@ class ObjectDetection:
         return video
 
     def display(video):
+        greyLower = np.array([0, 0, 171])
+        greyUpper = np.array([255, 255, 255])
+        pts = deque(maxlen=128)
 
         while(True):
             ret, Frame = video.read()
-            cv2.imshow('frame', Frame)
+
+            frame = imutils.resize(Frame, width=600)
+            blurred = cv2.GaussianBlur(Frame, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+            mask = cv2.inRange(hsv, greyLower, greyUpper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+
+            cv2.imshow('frame', mask)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
